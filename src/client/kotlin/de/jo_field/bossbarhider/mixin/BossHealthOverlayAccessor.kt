7d7a02ed878d48edc99ad32593e92fc368edf428
Jo_field <150937665+JoField08@users.noreply.github.com>
2026-08-15 12:26:05 +0200

    💄 style(BossHealthOverlay): remove outdated comments from accessor interface

--- src/client/kotlin/de/jo_field/bossbarhider/mixin/BossHealthOverlayAccessor.kt
+++ src/client/kotlin/de/jo_field/bossbarhider/mixin/BossHealthOverlayAccessor.kt
@@ -6,14 +6,6 @@ import org.spongepowered.asm.mixin.Mixin
 import org.spongepowered.asm.mixin.gen.Accessor
 import java.util.UUID
 
-/**
- * Accessor-Mixin: gibt uns Lese-/Schreibzugriff auf das private Feld,
- * in dem die BossHealthOverlay die aktuell aktiven BossBars haelt.
- *
- * Verifiziert gegen die im Projekt gebundelte MC-26.2-Client-Jar:
- * net.minecraft.client.gui.components.BossHealthOverlay#events
- * vom Typ Map<UUID, LerpingBossEvent>.
- */
 @Mixin(BossHealthOverlay::class)
 interface BossHealthOverlayAccessor {
 
