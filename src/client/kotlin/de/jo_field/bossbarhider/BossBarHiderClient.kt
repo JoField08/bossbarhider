b5e038561ef25f37afeca2924a6c69e7fa7e261e
Jo_field <150937665+JoField08@users.noreply.github.com>
2026-09-07 17:06:11 +0200

    delete unecessary command

--- src/client/kotlin/de/jo_field/bossbarhider/BossBarHiderClient.kt
+++ src/client/kotlin/de/jo_field/bossbarhider/BossBarHiderClient.kt
@@ -12,7 +12,6 @@ class BossBarHiderClient : ClientModInitializer {
     }
 
     override fun onInitializeClient() {
-        BossBarsCommand.register()
         BossBarHiderConfig.load()
         LOGGER.info("BossBar Hider initialized ({} rules, enabled={})",
             BossBarHiderConfig.current.rules.size, BossBarHiderConfig.current.isHiderEnabled)
